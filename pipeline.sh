d238631b70fa1bf9972727c89d04d981b5481a36
paulhectork <paul.kervegan@hotmail.fr>
2025-05-09 23:58:30 +0200

    pipeline script

--- pipeline.sh
+++ pipeline.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# ---------------------------------------
+# variables
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 VENV_DIR="$SCRIPT_DIR/venv"
@@ -11,10 +14,48 @@ DATA_DTLR_DIR="$SCRIPT_DIR"/data_character_detection
 LP_DIR="$SCRIPT_DIR"/LinePredictor
 DTLR_DIR="$SCRIPT_DIR"/DTLR
 
+SAMPLE=False
+
+# ---------------------------------------
+# functions
+
+print_usage() {
+    cat<<EOF
+
+USAGE bash pipeline.sh [-s]
+
+    run the character detection pipeline
+
+    -s : run the pipeline on a sample of 10 images (instead of 6000+ images)
+
+EOF
+}
+
+# ---------------------------------------
+# process
+
+while getopts 's' flag; do
+    case "${flag}" in
+        s) SAMPLE=True;;
+        *) print_usage
+           exit 1;;
+    esac
+done
+
 if [ ! -d "$VENV_DIR" ]; then echo "virtual env 'venv' not found (at '$VENV_DIR'). exiting" && exit 1; fi
 
 source "$VENV_DIR"/bin/activate
 
-python "$LP_DIR"/pipeline_inference.py\
-    -i "$DATA_DIR"\
-    -o "$DATA_LP_DIR"
+# step 1: character detection
+if "$SAMPLE"; then
+    python "$LP_DIR"/pipeline_inference.py\
+        -i "$DATA_DIR"\
+        -o "$DATA_LP_DIR"\
+        -s
+else
+    python "$LP_DIR"/pipeline_inference.py\
+        -i "$DATA_DIR"\
+        -o "$DATA_LP_DIR"
+fi
+
+#TODO step 2
